d67d5b1cc1c86afa8f4d1f02af3fd07430cffdb8
Nicolas114 <gonzaleznicolas574@gmail.com>
2022-07-15 02:09:11 -0300

    Six commit

---
+++ 18_Sesiones_y_Cookies/cookie1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+    setcookie("prueba", "esta es la informacion de la primera cookie", time() + 500, "/CursoPHPildorasInformaticas/18_Sesiones_y_Cookies/zona_contenidos/");
+?>
+</body>
+</html>
---
+++ 18_Sesiones_y_Cookies/eliminar_cookie.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+//solo esta funcionando en el directorio indicado
+setcookie("prueba", "esta es la informacion de la primera cookie", time() - 1, "/CursoPHPildorasInformaticas/18_Sesiones_y_Cookies/zona_contenidos/");
+?>
+    
+</body>
+</html>
